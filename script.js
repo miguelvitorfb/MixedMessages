3c91cba6dfe9cbd829ccf1510d254c6c17c451d9
miguelvitorfb <miguelvitor2005@gmail.com>
2025-08-06 09:17:27 -0300

    First commit of the new randomizer

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
 const mixedMessages = () => {
     const outMessage = document.querySelector("span")
-    const random = Math.floor(Math.random() * 10)
-    switch (random) {
+    const random = Math.floor(Math.random() * 3)
 
-        case  0:
-            outMessage.textContent = "Por que o JavaScript foi ao terapeuta?Porque ele tinha muitos problemas com escopo."
-            break;
+    const ramOne = []
+    const ramTwo = []
+    const ramThree = []
 
-        case 1:
-            outMessage.textContent = "Como o programador sobreviveu ao apocalipse zumbi?Ele ficou em um loop infinito escondido."
-            break;
-
-        case 2:
-            outMessage.textContent = "O que um array disse para o outro?Me dá um espaço!"
-            break;
-
-        case 3:
-            outMessage.textContent = "Eu namorei um(a) front-end uma vez...Mas era só aparência."
-            break;
-
-        case 4:
-            outMessage.textContent = "Por que o programador morreu afogado?Porque foi nadar no Stack Overflow."
-            break;
-
-        case 5:
-            outMessage.textContent = "Qual o tipo de música que o código gosta?Lo-fi, porque sempre tá rodando em loop."
-            break;
-
-        case 6:
-            outMessage.textContent = "Tentei fazer um relacionamento dar certo…Mas a variável era const."
-            break;
-        case 7:
-            outMessage.textContent = "Por que o código terminou com a função?Ela só retornava problema."
-            break;
-
-        case 8: 
-            outMessage.textContent = "Git é como ex: você sabe que devia dar um commit, mas continua dando push nos conflitos."
-            break;
-
-        default:
-            outMessage.textContent = "O que acontece quando você tenta dividir por zero?O universo lança uma exception."
-            break;
-    }
+   
 }
 const btRandom = document.querySelector("button").addEventListener('click',mixedMessages)
 
